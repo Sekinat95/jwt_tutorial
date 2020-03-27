de6819721973d85c7cdb37073c35c600fc099a6f
sekinat95 <sekinat95@gmail.com>
2020-03-27 21:37:21 +0100

    pushing all of these here for now

---
+++ server.js
@@ -0,0 +1,54 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
+const posts = [
+    {
+        username: "sekinat",
+        title: "post 1"
+    },
+    {
+        username: "kyle",
+        title: "post 2"
+    }
+]
+
+//a simple route
+app.get('/posts',authenticateToken ,(req, res) => {
+   // res.json(posts)
+    res.json(posts.filter(post => post.username===req.user.name))
+})
+
+//the auth
+app.post('/login',(req, res) => {
+    //AUTHENTICATION (check this out later)
+
+    const username = req.body.username
+    const user = {name: username}
+
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({accessToken: accessToken})
+
+
+
+})
+
+//middleware to authenticate token
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1] //split bearer and token with a space. get token(second param)
+   // Bearer TOKEN
+   if(token==null) return res.sendStatus(401)
+
+   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+       if(err) return res.sendStatus(403)
+
+       req.user = user
+       next()
+   })
+}
+
+app.listen(3000)
